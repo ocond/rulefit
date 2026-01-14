3cfe81062c2e5075fef6520ee5240b5443db8d50
ocond <55338099+ocond@users.noreply.github.com>
2026-01-14 20:22:48 +0100

    Refactor print statements and change n_jobs value
    Updated print statements for better debugging and modified n_jobs parameter.

--- rulefit/rulefit.py
+++ rulefit/rulefit.py
@@ -1,146 +1,3 @@
-"""Linear model of tree-based decision rules
-
-This method implement the RuleFit algorithm
-
-The module structure is the following:
-
-- ``RuleCondition`` implements a binary feature transformation
-- ``Rule`` implements a Rule composed of ``RuleConditions``
-- ``RuleEnsemble`` implements an ensemble of ``Rules``
-- ``RuleFit`` implements the RuleFit algorithm
-
-"""
-import pandas as pd
-import numpy as np
-from sklearn.base import BaseEstimator
-from sklearn.base import TransformerMixin, RegressorMixin, ClassifierMixin
-from sklearn.ensemble import (
-    GradientBoostingRegressor,
-    GradientBoostingClassifier,
-    RandomForestRegressor,
-    RandomForestClassifier,
-)
-from sklearn.linear_model import LassoCV, LogisticRegressionCV
-from functools import reduce
-from ordered_set import OrderedSet
-
-
-class RuleCondition:
-    """Class for binary rule condition
-
-    Warning: this class should not be used directly.
-    """
-
-    def __init__(self, feature_index, threshold, operator, support, feature_name=None):
-        self.feature_index = feature_index
-        self.threshold = threshold
-        self.operator = operator
-        self.support = support
-        self.feature_name = feature_name
-
-    def __repr__(self):
-        return self.__str__()
-
-    def __str__(self):
-        if self.feature_name:
-            feature = self.feature_name
-        else:
-            feature = self.feature_index
-        return "%s %s %s" % (feature, self.operator, self.threshold)
-
-    def transform(self, X):
-        """Transform dataset.
-
-        Parameters
-        ----------
-        X: array-like matrix, shape=(n_samples, n_features)
-
-        Returns
-        -------
-        X_transformed: array-like matrix, shape=(n_samples, 1)
-        """
-        if self.operator == "<=":
-            res = 1 * (X[:, self.feature_index] <= self.threshold)
-        elif self.operator == ">":
-            res = 1 * (X[:, self.feature_index] > self.threshold)
-        return res
-
-    def __eq__(self, other):
-        return self.__hash__() == other.__hash__()
-
-    def __hash__(self):
-        return hash(
-            (self.feature_index, self.threshold, self.operator, self.feature_name)
-        )
-
-
-class Winsorizer:
-    """Performs Winsorization 1->1*
-
-    Warning: this class should not be used directly.
-    """
-
-    def __init__(self, trim_quantile=0.0):
-        self.trim_quantile = trim_quantile
-        self.winsor_lims = None
-
-    def train(self, X):
-        # get winsor limits
-        self.winsor_lims = np.ones([2, X.shape[1]]) * np.inf
-        self.winsor_lims[0, :] = -np.inf
-        if self.trim_quantile > 0:
-            for i_col in np.arange(X.shape[1]):
-                lower = np.percentile(X[:, i_col], self.trim_quantile * 100)
-                upper = np.percentile(X[:, i_col], 100 - self.trim_quantile * 100)
-                self.winsor_lims[:, i_col] = [lower, upper]
-
-    def trim(self, X):
-        X_ = X.copy()
-        X_ = np.where(
-            X > self.winsor_lims[1, :],
-            np.tile(self.winsor_lims[1, :], [X.shape[0], 1]),
-            np.where(
-                X < self.winsor_lims[0, :],
-                np.tile(self.winsor_lims[0, :], [X.shape[0], 1]),
-                X,
-            ),
-        )
-        return X_
-
-
-class FriedScale:
-    """Performs scaling of linear variables according to Friedman et al. 2005 Sec 5
-
-    Each variable is first Winsorized l->l*, then standardised as 0.4 x l* / std(l*)
-    Warning: this class should not be used directly.
-    """
-
-    def __init__(self, winsorizer=None):
-        self.scale_multipliers = None
-        self.winsorizer = winsorizer
-
-    def train(self, X):
-        # get multipliers
-        if self.winsorizer != None:
-            X_trimmed = self.winsorizer.trim(X)
-        else:
-            X_trimmed = X
-
-        scale_multipliers = np.ones(X.shape[1])
-        for i_col in np.arange(X.shape[1]):
-            num_uniq_vals = len(np.unique(X[:, i_col]))
-            if (
-                num_uniq_vals > 2
-            ):  # don't scale binary variables which are effectively already rules
-                scale_multipliers[i_col] = 0.4 / (1.0e-12 + np.std(X_trimmed[:, i_col]))
-        self.scale_multipliers = scale_multipliers
-
-    def scale(self, X):
-        if self.winsorizer != None:
-            return self.winsorizer.trim(X) * self.scale_multipliers
-        else:
-            return X * self.scale_multipliers
-
 
 class Rule:
     """Class for binary Rules from list of conditions
@@ -166,6 +23,7 @@ class Rule:
         -------
         X_transformed: array-like matrix, shape=(n_samples, 1)
         """
+        print("tf ", end = "")
         rule_applies = [condition.transform(X) for condition in self.conditions]
         return reduce(lambda x, y: x * y, rule_applies)
 
@@ -284,10 +142,14 @@ class RuleEnsemble:
         X_transformed: array-like matrix, shape=(n_samples, n_out)
             Transformed dataset. Each column represents one rule.
         """
+        print("Transforming RuleEnsemble")
         rule_list = list(self.rules)
+        print("Created rule_list")
         if coefs is None:
+            print("coefs is none")
             return np.array([rule.transform(X) for rule in rule_list]).T
         else:  # else use the coefs to filter the rules we bother to interpret
+            print("coefs is 0")
             res = np.array(
                 [
                     rule_list[i_rule].transform(X)
@@ -473,8 +335,8 @@ class RuleFit(BaseEstimator, TransformerMixin):
                 self.tree_generator.set_params(warm_start=True)
                 curr_est_ = 0
                 for i_size in np.arange(len(tree_sizes)):
-                    print(f"Fitting tree {curr_est_ +1}/{len(tree_sizes)}.")
                     size = tree_sizes[i_size]
+                    print(f"Fitting tree {curr_est_ +1}/{len(tree_sizes)} with tree size {size}.")
                     self.tree_generator.set_params(n_estimators=curr_est_ + 1)
                     self.tree_generator.set_params(max_leaf_nodes=size)
                     random_state_add = self.random_state if self.random_state else 0
@@ -702,7 +564,7 @@ class RuleFitRegressor(RuleFit, RegressorMixin, TransformerMixin):
         cv=3,
         tol=0.0001,
         max_iter=10000,
-        n_jobs=None,
+        n_jobs=3,
         random_state=None,
     ):
         super().__init__(
