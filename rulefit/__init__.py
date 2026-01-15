3b590100b66d86152621c715e50fa255c9f67ae7
ocond <oliver.conder@web.de>
2026-01-15 16:17:34 +0100

    test

--- rulefit/__init__.py
+++ rulefit/__init__.py
@@ -1,3 +1,3 @@
-from .rulefit import RuleCondition, Rule, RuleEnsemble, RuleFit, FriedScale, Winsorizer
+from .rulefit import RuleCondition, Rule, RuleEnsemble, RuleFit, FriedScale, Winsorizer, set_lasso
 
 __all__ = ["rulefit"]
