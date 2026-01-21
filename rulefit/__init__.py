4ca844fc1818088b2eed41f70dbff413b265f531
ocond <55338099+ocond@users.noreply.github.com>
2026-01-21 21:44:08 +0100

    Add RuleFitRegressor to imports in __init__.py

--- rulefit/__init__.py
+++ rulefit/__init__.py
@@ -1,3 +1,3 @@
-from .rulefit import RuleCondition, Rule, RuleEnsemble, RuleFit, FriedScale, Winsorizer, set_lasso
+from .rulefit import RuleCondition, Rule, RuleEnsemble, RuleFit, FriedScale, Winsorizer, set_lasso, RuleFitRegressor
 
 __all__ = ["rulefit"]
